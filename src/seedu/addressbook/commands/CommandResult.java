7182d7b2c9df18927f10856ef78b02f3985cc1e0
yeejfe <yeejfe@hotmail.com>
2017-02-08 16:40:03 +0800

    Ready to review

--- src/seedu/addressbook/commands/CommandResult.java
+++ src/seedu/addressbook/commands/CommandResult.java
@@ -10,7 +10,10 @@ import java.util.Optional;
  */
 public class CommandResult {
 
-    /** The feedback message to be shown to the user. Contains a description of the execution result */
+    /**
+     * The feedback message to be shown to the user. Contains a description of
+     * the execution result
+     */
     private final String feedbackToUser;
 
     /** The list of persons that was produced by the command */
@@ -33,6 +36,14 @@ public class CommandResult {
         return Optional.ofNullable(relevantPersons);
     }
 
+    /**
+     * Returns a String object that is the feedback to user.
+     * <p>
+     * This method always returns immediately, whether or not the feedback exists.
+     *
+     * @return the String feedback to user
+     * @see String
+     */
     public String getFeedbackToUser() {
         return feedbackToUser;
     }
